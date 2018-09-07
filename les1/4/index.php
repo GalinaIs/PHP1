ec333c48d25938fa74fb406e70047280097de6bb
Galina Is <denis@isyanov.com>
2018-09-07 15:51:01 +0300

    Создана страница html, используя переменные php.

---
+++ les1/4/index.php
@@ -0,0 +1,8 @@
+<?php
+    $h1Value = "Exercise 4";
+    $titleValue = "Lesson 2";
+    $year = date("Y"); 
+    echo "<h1>{$h1Value}</h1>";
+    echo "<head><title>{$titleValue}</title></head>";
+    echo "<p>Current year - {$year}</p>";
+?>
